b508ffbd89bd39000d8a7dd3c5c77a577ef6b365
Patrick Brandt <pbrandt@nrao.edu>
2018-10-02 10:52:44 -0600

    Split min/max and histogram calculations into their own classes.
    This actually provided a slight speedup for some images.

--- Region/RegionStats.cc
+++ Region/RegionStats.cc
@@ -1,5 +1,7 @@
 //# RegionStats.cc: implementation of class for calculating region statistics and histograms
 
+#include "Histogram.h"
+#include "MinMax.h"
 #include "RegionStats.h"
 
 #include <chrono>
@@ -53,57 +55,16 @@ void RegionStats::fillHistogram(CARTA::Histogram* histogram, const casacore::Mat
         if (numBins < 0) 
             numBins = int(max(sqrt(nrow * ncol), 2.0));
 
-        // Calculate min/max
-        float minVal, maxVal;
         tbb::blocked_range2d<size_t> range(0, ncol, 0, nrow);
-        std::tie(minVal, maxVal) = tbb::parallel_reduce(
-            range,
-            std::make_pair(std::numeric_limits<float>::max(), std::numeric_limits<float>::min()),
-            [&chanMatrix](const tbb::blocked_range2d<size_t> &r, const std::pair<float,float> &init) {
-                std::pair<float,float> mmv = init;
-                for(size_t j = r.rows().begin(); j != r.rows().end(); ++j) {
-                    for(size_t i = r.cols().begin(); i != r.cols().end(); ++i) {
-                        float val = chanMatrix(i,j);
-                        mmv.first = std::fmin(mmv.first, val);
-                        mmv.second = std::fmax(mmv.second, val);
-                    }
-                }
-                return mmv;
-            },
-            [](const std::pair<float,float> &x, const std::pair<float,float> &y) {
-                return std::make_pair(
-                    std::min(x.first, y.first), std::max(x.second, y.second));
-            });
+        MinMax<float> mm(chanMatrix);
+        tbb::parallel_reduce(range, mm);
+        float minVal, maxVal;
+        std::tie(minVal, maxVal) = mm.getMinMax();
 
-        float binWidth = (maxVal - minVal) / numBins;
-        std::vector<int> histogramBins = tbb::parallel_reduce(
-            range,
-            std::vector<int>(numBins, 0),
-            [&binWidth, &chanMatrix, &maxVal, &minVal, &numBins](const tbb::blocked_range2d<size_t> &r, const std::vector<int> &init) {
-                std::vector<int> hist(init);
-                for (auto j = r.rows().begin(); j != r.rows().end(); ++j) {
-                    for (auto i = r.cols().begin(); i != r.cols().end(); ++i) {
-                        auto v = chanMatrix(i,j);
-                        if (std::isnan(v))
-                            continue;
-                        int bin = std::max(std::min((int) ((v - minVal) / binWidth), numBins - 1), 0);
-                        ++hist[bin];
-                    }
-                }
-                return hist;
-            },
-            [&numBins](const std::vector<int> &a, const std::vector<int> &b) {
-                // TBF: This could probably be done more intelligently...
-                std::vector<int> c(numBins);
-                auto range = tbb::blocked_range<size_t>(0, numBins);
-                auto loop = [&](const tbb::blocked_range<size_t> &r) {
-                    size_t beg = r.begin();
-                    size_t end = r.end();
-                    std::transform(&a[beg], &a[end], &b[beg], &c[beg], std::plus<int>());
-                };
-                tbb::parallel_for(range, loop);
-                return c;
-            });
+        Histogram hist(numBins, minVal, maxVal, chanMatrix);
+        tbb::parallel_reduce(range, hist);
+        std::vector<int> histogramBins = hist.getHistogram();
+        float binWidth = hist.getBinWidth();
 
         // auto tEnd = std::chrono::high_resolution_clock::now();
         // auto dt = std::chrono::duration_cast<std::chrono::microseconds>(tEnd - tStart).count();
